71abf4a619f85d30b17fa60062a32bdadcba403c
Petr Cvengros <petr.cvengros@gooddata.com>
2015-03-06 16:31:51 -0800

    rscripts from s3

--- apps/r_brick/README.md
+++ apps/r_brick/README.md
@@ -5,8 +5,57 @@ Brick for executing R code.
 Data from specified ADS tables -> your R script -> data to specified ADS tables.
 
 ## Params
-- `R_SCRIPT_DIR` (String): Directory where the script is. Can be a local path - relative to the deployed app (e.g. `'r/scripts'`), or s3 (`s3://fdfd/fdfd`). In case s3 is used, you have to provide blah blah XXXX
+- `R_SCRIPT_DIR` (String): Directory where the script is. Can be a local path - relative to the deployed app (e.g. `'r/scripts'`), or s3 (`s3://bucket/path`). In case s3 is used, you have to provide blah blah XXXX `AWS_ACCESS_KEY_ID`, `AWS_SECRET_ACCESS_KEY`
 - `R_SCRIPT_FILENAME`
 - `ADS_ID`, `ADS_USERNAME`, `ADS_PASSWORD` (hidden): ADS credentials
 - `INPUT_TABLES` (Array): Names of tables that are to be fed into the R script. Each given table is exported to a CSV and given to the R script. E.g. if you want to predict sales in R based on the sales table, you'll pass ['sales'] to the brick. The table will be exported to `/tmp/something/sales.csv` which will be passed to your scripts in the `input_filenames` R variable. 
 - `OUTPUT_TABLES` (Array): Any output that your R script generates can be saved back to ADS. Save the results to the file that you got in the `output_tables` R variable. The order of filenames corresponds to order you provided in the OUTPUT_TABLES param. The brick will save the output data to ADS. E.g. you generated predictions and want it to be saved to the `predictions` table. You obtain the filename `/tmp/something/predictions.csv` from the `output_tables` variable. You write your results to the file. The brick will import the data to the `predictions` table in ADS.
+
+## Examples
+Example R script, that runs a prediction on top of a scenario data and saves the preditions (score) `predict.r`:
+```r
+suppressPackageStartupMessages(require(randomForest, quietly=TRUE))
+
+# read the scenario - the path comes in as the first member
+# of the input_filenames param
+scenario_file <- head(input_filenames, n=1)
+scenario <- read.csv(scenario_file)
+
+# load the model - path is relative to the directory 
+# given in R_SCRIPT_DIR param
+load('rfmodel.rmd')
+
+# predict the score 
+score <- predict(rf, scenario)
+
+# write it to a file given as the first member of the output_filenames param
+output_file <- head(output_filenames, n=1)
+write.csv(score, output_file)
+
+```
+Brick params when running R script contained in the brick folder, path is relative to the brick folder.
+```json
+{
+  "R_SCRIPT_DIR": "r_scripts/my",
+  "R_SCRIPT_FILENAME": "predict.r",
+  "ADS_ID": "ads id (just the last part)",
+  "ADS_USERNAME": "you@gooddata.com",
+  "ADS_PASSWORD": "your pass",
+  "INPUT_TABLES": "scenario",
+  "OUTPUT_TABLES": "score"
+}
+```
+Brick params when running R script that is on S3.
+```json
+{
+  "R_SCRIPT_DIR": "s3n://my-bucket/ha/r_scripts/my",
+  "R_SCRIPT_FILENAME": "predict.r",
+  "ADS_ID": "ads id (just the last part)",
+  "ADS_USERNAME": "you@gooddata.com",
+  "ADS_PASSWORD": "your pass",
+  "INPUT_TABLES": "scenario",
+  "OUTPUT_TABLES": "score",
+  "AWS_ACCESS_KEY_ID": "your id",
+  "AWS_SECRET_ACCESS_KEY": "your secret"
+}
+```
--- apps/r_brick/r_brick.rb
+++ apps/r_brick/r_brick.rb
@@ -1,5 +1,4 @@
 # encoding: utf-8
-require 'rinruby'
 require 'gooddata'
 require 'gooddata_datawarehouse'
 require 'tempfile'
@@ -26,21 +25,63 @@ module GoodData::Bricks
       Dir::Tmpname.create("#{table_name}.csv") { |path| path }
     end
 
+    def get_temp_file_folder
+      f = Dir::Tmpname.create("haha") { |path| path}
+      d = File.join(File.dirname(f), Time.now.to_i.to_s)
+      FileUtils.mkdir_p(d)
+      d
+    end
+
     def call(params)
       # get params from params
       config = params['config']
       r_script_dir = config['R_SCRIPT_DIR'] or fail 'R_SCRIPT_DIR is empty. You need to provide path where the R script is'
       r_script_filename = config['R_SCRIPT_FILENAME'] or fail 'R_SCRIPT_FILENAME is empty. You need to provide the filename of the R script'
       output_tables = config['OUTPUT_TABLES']
+      output_filenames = nil
       input_tables = config['INPUT_TABLES']
+      input_filenames = nil
       ads_id = config['ADS_ID']
       ads_username = config['ADS_USERNAME']
       ads_password = config['ADS_PASSWORD']
 
       # if the files are at s3, get them
       if /s3n:/ =~ r_script_dir
+        id = config['AWS_ACCESS_KEY_ID']
+        secret = config['AWS_SECRET_ACCESS_KEY']
+
         # download it from there
         # r_script_dir = where it was downloaded
+        uri = URI(r_script_dir)
+        bucket_name = uri.host
+        bucket_path = uri.path[1..-1]
+
+        s3 = AWS::S3.new(
+          :access_key_id => id,
+          :secret_access_key => secret
+        )
+        temp_dir = get_temp_file_folder
+
+        # list the keys and download the files
+        bucket = s3.buckets[bucket_name]
+        bucket.objects.with_prefix(bucket_path).each do |obj|
+          f = obj.key
+          # do nothing for dirs
+          if f[-1] == '/'
+            next
+          end
+
+          # download the files from s3 to temp folder
+          file_path = File.join(temp_dir, f)
+          FileUtils.mkdir_p(File.dirname(file_path))
+          # streaming download from S3 to a file on disk
+          File.open(file_path, 'wb') do |file|
+            obj.read do |chunk|
+               file.write(chunk)
+            end
+          end
+        end
+        r_script_dir = File.join(temp_dir, bucket_path)
       end
 
       # if input tables given export stuff from ADS to CSV
@@ -57,8 +98,6 @@ module GoodData::Bricks
           dwh.export_table(table_name, f)
           input_filenames << f
         end
-        # pass it to the script
-        R.input_filenames = input_filenames
       end
 
       # if output tables given, create temporary files for them
@@ -72,12 +111,20 @@ module GoodData::Bricks
           output_filenames << f
         end
 
-        # pass it to the script
-        R.output_filenames = output_filenames
       end
 
-      # run the script
-      R.eval(File.read(File.join(r_script_dir, r_script_filename)))
+      # run the script in the right folder
+      FileUtils.cd(r_script_dir) do
+        require 'rinruby'
+
+        # pass the filenames to the script
+        R.input_filenames = input_filenames if input_filenames
+        R.output_filenames = output_filenames if output_filenames
+
+        # run the script
+        R.eval(File.read(File.join(r_script_dir, r_script_filename)))
+      end
+
       # if output_tables given load it to ads
       unless output_tables.nil? || output_tables.empty?
         dwh = get_dwh(ads_username, ads_password, ads_id)
